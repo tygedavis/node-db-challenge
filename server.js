779e621fd5796c456cf00eacdcbdbfa1c88ba9a5
tygedavis <tyge.davis@gmail.com>
2020-01-27 08:56:12 -0700

    Fixed typos with docs endpoint

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const server = express();
 server.use(express.json());
 
 server.use('/', projRouter);
-server.use('.docs', express.static('./docs'))
+server.use('/docs', express.static('./docs'))
 
 module.exports = server;
